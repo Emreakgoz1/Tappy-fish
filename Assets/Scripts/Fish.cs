cfbad1252c348cfe7e9bd5535e27a63e6e7945df
Baran Emre <113918403+DevEmreLngnotion@users.noreply.github.com>
2023-01-09 16:31:48 +0300

    Fish angle added

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,21 +4,52 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D _rb;
     public float _speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        FishSwim();
+        FishRotatin();
+      
+    }
+
+    public void FishSwim()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            rb.velocity = Vector2.zero;
-            rb.velocity = new Vector2(rb.velocity.x,_speed);
+            _rb.velocity = Vector2.zero;
+            _rb.velocity = new Vector2(_rb.velocity.x, _speed);
         }
     }
+    public void FishRotatin()
+    {
+        if (_rb.velocity.y > 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+
+        }
+        else if (_rb.velocity.y<-1.2f)
+        {
+            if (angle>minAngle)
+            {
+                angle = angle - 2;
+            }
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
 }
